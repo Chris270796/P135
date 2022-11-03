205711094e651df6e405b5a15919a90c5cd53798
Chris270796 <84269447+Chris270796@users.noreply.github.com>
2022-11-03 15:58:09 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,77 +1,54 @@
-timer_counter = 0;
-timer_check = "";
-drawn_sketch = "";
-answer_holder = "";
-score = 0;
-
-
-function updateCanvas() {
-    background("white");
-    random_number = Math.floor((Math.random() * quick_draw_data_set.length) + 1);
-    console.log(quick_draw_data_set[random_number]);
-    sketch = quick_draw_data_set[random_number];
-    document.getElementById('sketch_name').innerHTML = 'Sketch To be Drawn: ' + sketch;
-}
-
-function preload() {
-    classifier = ml5.imageClassifier('DoodleNet');
-}
+status = "";
+objects = [];
+target = "";
 
 function setup() {
-    canvas = createCanvas(340, 340);
+    canvas = createCanvas(500, 400);
     canvas.center();
-    background("white");
-    canvas.mouseReleased(classifyCanvas);
+    video = createCapture(VIDEO);
+    video.size(500, 400);
+    video.hide();
 }
 
+function start() {
+    objectDetector = ml5.objectDetector("cocosd", modelLoaded);
+    document.getElementById("status").innerHTML = "Status : Detecting Objects";
+    target = document.getElementById("input").value;
+    if (target == objects[i].label) {
+        video.stop();
+        document.getElementById("status").innerHTML = target + "Found"
+    }
+
+}
+
+
+function modelLoaded() {
+    console.log("Model Loaded!");
+    status = true;
+    video.loop();
+    video.speed(1);
+    video.volume(1);
+}
 
 function draw() {
-    strokeWeight(13);
-    stroke(0);
-    if (mouseIsPressed) {
-        line(pmouseX, pmouseY, mouseX, mouseY);
-    }
-    check_sketch()
-    if (drawn_sketch == sketch) {
-        answer_holder = "set"
-        score++;
-        document.getElementById('score').innerHTML = 'Score: ' + score;
-    }
+    image(video, 0, 0, 500, 400);
+    if (status != "") {
+        for (i = 0; i < objects.length; i++) {
+            fill("#FF0000");
+            percent = floor(objects[i].confidence * 100);
+            text(objects[i].label + " " + percent + "%", objects[i].x + 15, objects[i].y + 15);
+            noFill();
+            stroke("#FF0000");
+            rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
 
+        }
+    }
 }
 
-function classifyCanvas() {
-    classifier.classify(canvas, gotResult);
-}
-
-function gotResult(error, results) {
+function gotResults(error, results) {
     if (error) {
         console.error(error);
     }
     console.log(results);
-    drawn_sketch = results[0].label;
-    document.getElementById('label').innerHTML = 'Your Sketch: ' + drawn_sketch;
-
-    document.getElementById('confidence').innerHTML = 'Confidence: ' + Math.round(results[0].confidence * 100) + '%';
-}
-
-
-function check_sketch() {
-    timer_counter++;
-    document.getElementById('time').innerHTML = 'Timer: ' + timer_counter;
-    console.log(timer_counter)
-    if (timer_counter > 700) {
-        timer_counter = 0;
-        timer_check = "completed"
-    }
-    if (timer_check == "completed" || answer_holder == "set") {
-        timer_check = "";
-        answer_holder = "";
-        updateCanvas();
-    }
-
-}
-
-function Clear() {
-    background("white");
+    objects = results;
 }
